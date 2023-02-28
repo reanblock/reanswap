caa72225d66f244164690ac9b736eaed4cef13b4
Darren Jensen <darren.jensen@gmail.com>
2023-02-28 17:52:30 +0700

    added getAmountsOut for supporting token swap paths

--- test/ReanswapV2Library.t.sol
+++ test/ReanswapV2Library.t.sol
@@ -133,4 +133,44 @@ contract ReanswapV2LibraryTest is Test {
         vm.expectRevert(encodeError("InsufficientLiquidity()"));
         ReanswapV2Library.getAmountOut(1000, 1 ether, 0);
     }
+
+    function testGetAmountsOut() public {
+        tokenA.transfer(address(pair), 1 ether);
+        tokenB.transfer(address(pair), 2 ether);
+        pair.mint(address(this));
+
+        tokenB.transfer(address(pair2), 1 ether);
+        tokenC.transfer(address(pair2), 0.5 ether);
+        pair2.mint(address(this));
+
+        tokenC.transfer(address(pair3), 1 ether);
+        tokenD.transfer(address(pair3), 2 ether);
+        pair3.mint(address(this));
+
+        address[] memory path = new address[](4);
+        path[0] = address(tokenA);
+        path[1] = address(tokenB);
+        path[2] = address(tokenC);
+        path[3] = address(tokenD);
+
+        uint256[] memory amounts = ReanswapV2Library.getAmountsOut(
+            address(factory),
+            0.1 ether,
+            path
+        );
+
+        assertEq(amounts.length, 4);
+        assertEq(amounts[0], 0.1 ether);
+        assertEq(amounts[1], 0.181322178776029826 ether);
+        assertEq(amounts[2], 0.076550452221167502 ether);
+        assertEq(amounts[3], 0.141817942760565270 ether);
+    }
+
+    function testGetAmountsOutInvalidPath() public {
+        address[] memory path = new address[](1);
+        path[0] = address(tokenA);
+
+        vm.expectRevert(encodeError("InvalidPath()"));
+        ReanswapV2Library.getAmountsOut(address(factory), 0.1 ether, path);
+    }
 }
